a7b214bf52af8aeaee9eb4c986b575328dc3a512
Anika Faiza <faizanika15@gmail.com>
2020-04-15 12:56:51 +0600

    docker added

--- API/test_app.py
+++ API/test_app.py
@@ -1,22 +1,19 @@
 from app import app, db
+from app import User, Book
 import os
 import unittest
+from unittest.mock import patch
 
 class TestMyApp(unittest.TestCase):
 
     def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
         self.tester = app.test_client()
-        db.create_all()
     
     def test_getting_all_books(self):
         #tester = app.test_client(self)
         resp = self.tester.get('/book')
-        status_code = resp.status_code
-        self.assertEqual(status_code, 401)
+        #status_code = resp.status_code
+        self.assertEqual(resp.status_code, 401)
 
     #check if the content returned is application/json
     def test_content(self):
@@ -28,7 +25,6 @@ class TestMyApp(unittest.TestCase):
         tester = app.test_client(self)
         resp = tester.get('/book')
         self.assertTrue(b'author' in resp.data)'''
-
 
 if __name__ == '__main__':
     unittest.main()
